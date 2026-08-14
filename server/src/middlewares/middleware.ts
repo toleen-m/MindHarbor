025db782bb9824a9d230085a3a858d6ab7a8c43b
Bhatti Unaiza Ali <unaizaab@gmail.com>
2026-08-14 13:43:27 -0400

    authetification: logout

---
+++ server/src/middlewares/middleware.ts
@@ -0,0 +1,39 @@
+import { type Request, type Response, type NextFunction } from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+import jwt from "jsonwebtoken"
+
+export function authentifier(req: Request, res: Response, next: NextFunction) {
+    const header = req.headers.authorization // Bearer xxx.yyy.zzz
+    if (!header?.startsWith("Bearer ")) {
+        return res.status(401).json({
+            erreur: "Token manquant"
+        })
+    }
+
+    const token = header.split(" ")[1]!
+
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET!)
+        ;(req as any).user = payload
+        next()
+    } catch {
+        res.status(401).json({
+            erreur: "Token invalide / expiré"
+        })
+    }
+}
+
+
+export function exigerRole(role: String) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if ((req as any).user.role !== role) {
+            return res.status(403).json({
+                erreur: "Accès refusé"
+            })
+        }
+        next()
+    }
+}
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,27 +1,19 @@
-import "dotenv/config"
+
 import { Router, type Request, type Response } from "express"
 import bcrypt from "bcryptjs"
+import prisma from "../../utils/prisma.js"
 import jwt from "jsonwebtoken"
-import { PrismaClient } from "../../generated/prisma/client.js"
-import { PrismaPg } from "@prisma/adapter-pg"
+import { authentifier, exigerRole } from "../middlewares/middleware.js"
 
 const routerAuth = Router()
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
-})
-
-const prisma = new PrismaClient({
-    adapter
-})
-
-
+// POST localhost:3000/auth/register -> Créer un compte
 routerAuth.post("/register", async (req: Request, res: Response) => {
-    const { nom, email, password } = req.body
+    const { email, password } = req.body
 
-    if (!nom || !email || !password) {
+    if (!email || !password) {
         return res.status(400).json({
-            erreur: "Nom, email ou mot de passe manquant !"
+            erreur: "mail ou mot de passe manquant !"
         })
     }
 
@@ -30,29 +22,97 @@ routerAuth.post("/register", async (req: Request, res: Response) => {
 
         const user = await prisma.user.create({
             data: {
-                nom,
-                email,
-                password: pass_hash,
-                avatar: "",
-                biographie: ""
-            }
+                  nom: "Utilisateur",
+                  email,
+                  password: pass_hash,
+                  avatar: "",
+                  biographie: ""
+          }
         })
 
      
         res.status(201).json({
             id: user.id,
-            nom: user.nom,
             email: user.email,
             role: user.role,
             createdAt: user.createdAt
         })
-} catch (error) {
-    console.log(error)
 
-    res.status(400).json({
-        erreur: "Erreur lors de la création"
-    })
-}
+    } catch {
+        res.status(400).json({
+            erreur: "Email existe deja !"
+        })
+    }
 })
 
+// POST localhost:3000/auth/login -> Se connecter
+routerAuth.post("/login", async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const user = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if (!user) {
+        return res.status(401).json({
+            erreur: "Identifiant invalides"
+        })
+    }
+
+    const ok = await bcrypt.compare(password, user.password)
+
+    if (!ok) {
+        return res.status(401).json({
+            erreur: "Mot de passe incorrect"
+        })
+    }
+
+    const token = jwt.sign(
+        {
+            sub: user.id,
+            role: user.role
+        },
+        process.env.JWT_SECRET!,
+        {
+            expiresIn: "1h"
+        }
+    )
+
+    res.json({ token })
+})
+
+routerAuth.get(
+    "/me",
+    authentifier,
+    (req: Request, res: Response) => {
+        res.json({
+            message: "Authentifié !",
+            utilisateur: (req as any).user
+        })
+    }
+)
+
+
+routerAuth.get(
+    "/admin-test",
+    authentifier,
+    exigerRole("ADMINISTRATEUR"),
+    (req: Request, res: Response) => {
+        res.json({
+            message: "Accès administrateur autorisé"
+        })
+    }
+)
+
+// POST localhost:3000/auth/logout -> Se déconnecter
+routerAuth.post(
+    "/logout",
+    (req: Request, res: Response) => {
+        res.json({
+            message: "Déconnexion réussie"
+        })
+    }
+)
+
 export default routerAuth
+
