52b7bcb21660cc3520680e16d098d570a7d6d813
Bhatti Unaiza Ali <unaizaab@gmail.com>
2026-08-15 19:33:42 -0400

    frontend fini -unaiza

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,24 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
 import routerAuth from "./routes/auth.routes.js"
 import routerUser from "./routes/user.routes.js"
 import routerMessage from "./routes/messages.routes.js"
-
 import journalRouter from "./routes/journal.routes.js"
-
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+
 app.use(express.json())
+
 app.use("/users", routerUser)
 app.use("/messages", routerMessage)
-
 app.use("/auth", routerAuth)
-
 
 
 app.use("/journal", journalRouter)
