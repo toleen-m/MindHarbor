83741750029cd2086ead01c7ea7d249703bd70fa
toleen-m <msabeetl@gmail.com>
2026-08-14 21:22:43 -0400

    add: express.d.ts

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,11 @@
+import type { Role } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: { id: string; role: Role };
+    }
+  }
+}
+
+export {};
